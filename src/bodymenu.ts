98093bf587dcbc409c8337a69d5a28ccc05ec4ba
Mathieu M <mmoiroux911@gmail.com>
2024-12-05 22:31:58 +0100

    Ajout du type BodyEntry et modification du constructeur BodyMenu

--- src/bodymenu.ts
+++ src/bodymenu.ts
@@ -1,13 +1,23 @@
-import {createElement} from "./elementFactory"
+import { createElement } from "./elementFactory"
+
+export interface BodyEntry {
+    coordWidth: number
+    coordHeight: number
+    callBack: () => void
+}
+// let v: BodyEntry = {coordHeight: 1, coordWidth: 2, callBack: (a) => a}
 
 export class BodyMenu {
     #root: HTMLElement
+    #entries: BodyEntry[]
 
-    constructor() {
-        this.#root = createElement('div', {"id": "bodyMenu"})
+    constructor(entries: BodyEntry[]) {
+        this.#root = createElement('div', { "id": "bodyMenu" })
+        this.#entries = entries
     }
 
     getRoot(): HTMLElement {
         return this.#root
     }
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import './style.css'
-import {BodyMenu} from "./bodymenu"
+import { BodyMenu, BodyEntry } from "./bodymenu"
 
 // Ts ...
+const items: BodyEntry[] = [
+    {coordHeight: 1, coordWidth: 2, callBack: () => {console.log("Callback 1")}},
+    {coordHeight: 3, coordWidth: 4, callBack: () => {console.log("Callback 2")}},
+    {coordHeight: 5, coordWidth: 6, callBack: () => {console.log("Callback 3")}}
+]
 
-document.querySelector("#pageContent")?.insertAdjacentElement("beforeend", new BodyMenu().getRoot())
+document.querySelector("#pageContent")?.insertAdjacentElement("beforeend", new BodyMenu(items).getRoot())
