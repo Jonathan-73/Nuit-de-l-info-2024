2d09fa99ab7cdaab62dc39eb679f7bc4cd4d1083
Swan <swan.auvergne@gmail.com>
2024-12-05 22:42:19 +0100

    navigationLayer

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -11,9 +11,9 @@ if (!token || !tokenHash || btoa(token) !== tokenHash) {
 }
 
 const items: BodyEntry[] = [
-  {coordHeight: 1, coordWidth: 2, callBack: () => {console.log("Callback 1")}},
-  {coordHeight: 3, coordWidth: 4, callBack: () => {console.log("Callback 2")}},
-  {coordHeight: 5, coordWidth: 6, callBack: () => {console.log("Callback 3")}}
+  {coordHeight: .1, coordWidth: .2, callBack: () => {console.log("Callback 1")}},
+  {coordHeight: .3, coordWidth: .4, callBack: () => {console.log("Callback 2")}},
+  {coordHeight: 1, coordWidth: 1, callBack: () => {console.log("Callback 3")}}
 ]
 
 document.querySelector("#pageContent")?.insertAdjacentElement("beforeend", new BodyMenu(items).getRoot())
