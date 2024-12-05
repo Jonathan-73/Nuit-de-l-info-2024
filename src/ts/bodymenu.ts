ed5b546db421fd2cda5b470c0a6bbfad2202d246
Swan <swan.auvergne@gmail.com>
2024-12-05 22:58:44 +0100

    MenuEntryRoot navigation & click

--- src/ts/bodymenu.ts
+++ src/ts/bodymenu.ts
@@ -1,29 +1,33 @@
 import { createElement } from "./elementFactory"
 
 export interface BodyEntry {
-    coordWidth: number
-    coordHeight: number
-    callBack: () => void
+    widthRatio: number
+    heightRatio: number
+    callback: () => void
 }
-// let v: BodyEntry = {coordHeight: 1, coordWidth: 2, callBack: (a) => a}
+// let bodyEntry: BodyEntry = {coordHeight: 1, coordWidth: 2, callBack: (a) => a}
 
 export class BodyMenu {
     #root: HTMLElement
     #entries: BodyEntry[]
 
     constructor(entries: BodyEntry[]) {
-        this.#root = createElement('div', { "id": "bodyMenu" })
+        this.#root = createElement('div', { id: "bodyMenu" })
         this.#entries = entries
 
         // Background
-        this.#root.insertAdjacentElement("afterbegin", createElement('div', { 'style': 'width: 100px; height: 100px; background-color: lightgrey;' }))
+        this.#root.insertAdjacentElement("afterbegin", createElement('div', { style: 'width: 100px; height: 100px; background-color: lightgrey;' }))
 
         // Navigation
-        let navigationLayer = createElement('div', { 'id': 'navigationLayer' })
+        let navigationLayer = createElement('div', { id: 'navigationLayer' })
         this.#root.insertAdjacentElement("beforeend", navigationLayer)
 
         for (const entry of this.#entries)
-            navigationLayer.insertAdjacentElement('beforeend', createElement('div', { 'class': 'MenuEntryRoot', 'style': `--width-ratio: ${entry.coordWidth}; --height-ratio: ${entry.coordHeight};` }))
+        {
+            let button = createElement('button', { class: 'MenuEntryRoot', style: `--width-ratio: ${entry.widthRatio}; --height-ratio: ${entry.heightRatio};`, type: 'button' })
+            button.addEventListener('click', entry.callback)
+            navigationLayer.insertAdjacentElement('beforeend', button)
+        }
     }
 
     getRoot(): HTMLElement {
