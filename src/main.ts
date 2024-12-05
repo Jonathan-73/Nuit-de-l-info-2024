8904dd2730a0bf45272b3b5c620eeb5167515c95
Swan <swan.auvergne@gmail.com>
2024-12-05 22:58:44 +0100

    MenuEntryRoot navigation & click

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import { BodyMenu, BodyEntry } from "./bodymenu"
 
 // Ts ...
 const items: BodyEntry[] = [
-    {coordHeight: .1, coordWidth: .2, callBack: () => {console.log("Callback 1")}},
-    {coordHeight: .3, coordWidth: .4, callBack: () => {console.log("Callback 2")}},
-    {coordHeight: 1, coordWidth: 1, callBack: () => {console.log("Callback 3")}}
+    {heightRatio: .1, widthRatio: .2, callback: () => {console.log("Callback 1")}},
+    {heightRatio: .3, widthRatio: .4, callback: () => {console.log("Callback 2")}},
+    {heightRatio: 1, widthRatio: 1, callback: () => {console.log("Callback 3")}}
 ]
 
 document.querySelector("#pageContent")?.insertAdjacentElement("beforeend", new BodyMenu(items).getRoot())
