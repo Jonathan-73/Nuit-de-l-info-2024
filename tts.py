67cf0ed3cf85d3af03f11f5f7adda84f24edddaa
ArpenteurDivin <jonath.dumaz73@gmail.com>
2024-12-06 02:53:18 +0100

    des bêtises

--- tts.py
+++ tts.py
@@ -4,7 +4,7 @@ from gtts import gTTS
 text = "Pierre Schuller... Veux-tu que je te Rondoudou le Rattata ?"
 
 # Créer un objet gTTS
-tts = gTTS(text=text, lang='es')
+tts = gTTS(text=text, lang='ro')
 
 # Sauvegarder l'audio dans un fichier
-tts.save("fichiers_mp3/output_es.mp3")
+tts.save("fichiers_mp3/output_ro.mp3")
