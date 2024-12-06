1eb728032d0d6aa4132a264974cd2307b132c50d
Charles Damaggio <charles.damaggiocrozier@gmail.com>
2024-12-06 01:50:53 +0100

    Added slider volume module + setup a slider music volume

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,4 +1,18 @@
 import '/src/css/style.css'
-
+import {createDivVolumeSlider} from './volumeSlider'
+import musique from '../assets/Μουσική/OceanWaves.mp3'
 // Ts ...
+let sliderVolume = createDivVolumeSlider();
 
+let oceanWaves = document.createElement("audio");
+oceanWaves.src = musique;
+oceanWaves.loop = true;
+oceanWaves.onloadeddata = () => oceanWaves.play();
+
+sliderVolume.addEventListener('change', ((evt: CustomEvent) => {
+    let volume = <number>evt.detail;
+    oceanWaves.volume = volume;
+}) as EventListener)
+
+document.body.append(sliderVolume);
+document.body.appendChild(oceanWaves);
