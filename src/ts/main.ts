a8a4476080844d1c3597202b949dcf590ff63640
Anastasios TSIOMPANIDIS <anastasios.tsiompanidis@etu.univ-grenoble-alpes.fr>
2024-12-06 01:49:31 +0100

    feat: :sparkles: Add new fishing activity
    Small refactor for main ts code, added the fishing activity and an example usage of the component activity in main

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,4 +1,23 @@
-import '/src/css/style.css'
+import "/src/css/style.css";
+
+import { createFishingGameComponent, startGame } from "./fishing";
+const token = sessionStorage.getItem("captchaToken");
+const tokenHash = sessionStorage.getItem("captchaHash");
+
+if (!token || !tokenHash || btoa(token) !== tokenHash) {
+  window.location.href = "/captcha.html";
+}
+
+// Possible example of an activiry component
+document.addEventListener("DOMContentLoaded", () => {
+  const appRoot = document.getElementById("app");
+  if (appRoot) {
+    const gameComponent = createFishingGameComponent();
+    appRoot.appendChild(gameComponent);
+    startGame();
+  } else {
+    console.error("#app element not found.");
+  }
+});
 
 // Ts ...
-
