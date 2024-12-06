8a8057635d758ad32177cd1b4f8d1d9b11cdc796
Charles Damaggio <charles.damaggiocrozier@gmail.com>
2024-12-06 06:29:42 +0100

    revert main changes

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,40 +1,40 @@
 import '/src/css/style.css'
 import { createDivVolumeSlider } from './volumeSlider'
-import {createCompositionDiv} from './composition'
+import {createCompositionDiv } from './composition'
 import musique from '../assets/Μουσική/OceanWaves.mp3'
 import { BodyMenu, BodyEntry } from "./bodymenu"
 
-// const token = sessionStorage.getItem("captchaToken");
-// const tokenHash = sessionStorage.getItem("captchaHash");
+const token = sessionStorage.getItem("captchaToken");
+const tokenHash = sessionStorage.getItem("captchaHash");
 
-// if (!token || !tokenHash || btoa(token) !== tokenHash) {
-//   window.location.href = "/captcha.html";
-// }
+if (!token || !tokenHash || btoa(token) !== tokenHash) {
+  window.location.href = "/captcha.html";
+}
 
-// const items: BodyEntry[] = [
-//   { coordRatio: { heightRatio: .125, widthRatio: .5 }, callback: () => { console.log("Tête") }, accessFromCoordRatio: { heightRatio: .23, widthRatio: .5 } },
-//   { coordRatio: { heightRatio: .42, widthRatio: .33 }, callback: () => { console.log("Bras côté gauche") }, accessFromCoordRatio: { heightRatio: .3, widthRatio: .4 } },
-//   { coordRatio: { heightRatio: .38, widthRatio: .46 }, callback: () => { console.log("Poumons") } },
-//   { coordRatio: { heightRatio: .35, widthRatio: .53 }, callback: () => { console.log("Coeur") } },
-//   { coordRatio: { heightRatio: .42, widthRatio: 1 - .33 }, callback: () => { console.log("Peau (bras côté droite)") }, accessFromCoordRatio: { heightRatio: .3, widthRatio: .6 } },
-//   { coordRatio: { heightRatio: .47, widthRatio: 0.45 }, callback: () => { console.log("Foie") } },
-//   // { coordRatio: { heightRatio: .76, widthRatio: 0.435 }, callback: () => { console.log("Jambe côté gauche") }, accessFromCoordRatio: { heightRatio: .56, widthRatio: 0.435 } },
-//   // { coordRatio: { heightRatio: .76, widthRatio: 1-0.435 }, callback: () => { console.log("Jambe côté droite") }, accessFromCoordRatio: { heightRatio: .56, widthRatio: 1-0.435 } },
-// ]
+const items: BodyEntry[] = [
+  { coordRatio: { heightRatio: .125, widthRatio: .5 }, callback: () => { console.log("Tête") }, accessFromCoordRatio: { heightRatio: .23, widthRatio: .5 } },
+  { coordRatio: { heightRatio: .42, widthRatio: .33 }, callback: () => { console.log("Bras côté gauche") }, accessFromCoordRatio: { heightRatio: .3, widthRatio: .4 } },
+  { coordRatio: { heightRatio: .38, widthRatio: .46 }, callback: () => { console.log("Poumons") } },
+  { coordRatio: { heightRatio: .35, widthRatio: .53 }, callback: () => { console.log("Coeur") } },
+  { coordRatio: { heightRatio: .42, widthRatio: 1 - .33 }, callback: () => { console.log("Peau (bras côté droite)") }, accessFromCoordRatio: { heightRatio: .3, widthRatio: .6 } },
+  { coordRatio: { heightRatio: .47, widthRatio: 0.45 }, callback: () => { console.log("Foie") } },
+  // { coordRatio: { heightRatio: .76, widthRatio: 0.435 }, callback: () => { console.log("Jambe côté gauche") }, accessFromCoordRatio: { heightRatio: .56, widthRatio: 0.435 } },
+  // { coordRatio: { heightRatio: .76, widthRatio: 1-0.435 }, callback: () => { console.log("Jambe côté droite") }, accessFromCoordRatio: { heightRatio: .56, widthRatio: 1-0.435 } },
+]
 
-// document.querySelector("#pageContent")?.insertAdjacentElement("beforeend", new BodyMenu(items).getRoot())
+document.querySelector("#pageContent")?.insertAdjacentElement("beforeend", new BodyMenu(items).getRoot())
 
-let sliderVolume = createCompositionDiv();
+let sliderVolume = createDivVolumeSlider();
 
-// let oceanWaves = document.createElement("audio");
-// oceanWaves.src = musique;
-// oceanWaves.loop = true;
-// oceanWaves.onloadeddata = () => oceanWaves.play();
+let oceanWaves = document.createElement("audio");
+oceanWaves.src = musique;
+oceanWaves.loop = true;
+oceanWaves.onloadeddata = () => oceanWaves.play();
 
-// sliderVolume.addEventListener('change', ((evt: CustomEvent) => {
-//   let volume = <number>evt.detail;
-//   oceanWaves.volume = volume;
-// }) as EventListener)
+sliderVolume.addEventListener('change', ((evt: CustomEvent) => {
+  let volume = <number>evt.detail;
+  oceanWaves.volume = volume;
+}) as EventListener)
 
 document.body.append(sliderVolume);
-// document.body.appendChild(oceanWaves);
+document.body.appendChild(oceanWaves);
